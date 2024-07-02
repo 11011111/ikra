f186901ea32bc956cee7f5f18f9bbb55ad37eda9
m11011111 <mk@elm-tech.ru>
2024-07-02 12:12:52 +0300

    stable web app v2

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -41,11 +41,4 @@ watch(
   }
 )
 
-
-const overflow = 100
-document.body.style.overflowY = 'hidden'
-document.body.style.marginTop = `${overflow}px`
-document.body.style.height = window.innerHeight + overflow + "px"
-document.body.style.paddingBottom = `${overflow}px`
-window.scrollTo(0, overflow)
 </script>
