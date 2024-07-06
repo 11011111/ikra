a5f9275d920ec52150e55d3a5a33b231068a3da3
m11011111 <mk@elm-tech.ru>
2024-07-06 14:31:56 +0300

    скорость икринок

--- src/components/AppTgPostCustom.vue
+++ src/components/AppTgPostCustom.vue
@@ -23,7 +23,7 @@ onMounted(() => {
       confetti("tsparticles", {
         spread: 80,
         ticks: 100,
-        gravity: -15,
+        gravity: -45,
         decay: 0.90,
         startVelocity: 10,
         particleCount: 5,
