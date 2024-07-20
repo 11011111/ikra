606e1f3a021f35bcf99d13ff3da327362ab72d46
m11011111 <mk@elm-tech.ru>
2024-07-20 20:46:27 +0300

    test

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -66,7 +66,7 @@ const tasksSuccess = computed(() => {
     AppTgPostCustom(v-model="actionPost" v-if="action" )
     Particles(id="tsparticles")
   .footer.z-isndex-priority.relative-position
-    .row.justify-between.items-center.wrap(v-if="!action")
+    .row.justify-between.items-center.wrap
       .block-element-footer.column.row.justify-between.items-center(@click="goTOP")
         .text-icon 🥇
         .l1-text.q-px-sm Рейтинг
